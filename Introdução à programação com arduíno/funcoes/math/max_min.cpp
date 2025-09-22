be1bbf0a01f8e0507656aebc04bf98372da569f0
João <jgablopes2008@gmail.com>
2025-09-22 09:49:11 -0300

    Ajustes

---
+++ Introdução à programação com arduíno/funcoes/math/max_min.cpp
@@ -0,0 +1,15 @@
+//MAX e MIN permitem retornar o maior e o menor número de um conjunto, respectivamente
+
+#include <iostream>;
+#include <math>;
+
+using namespace std;
+
+//max(5, 10) //retorna 10
+//min(5, 10) //retorna 5
+
+//para array e coleções
+
+//max_element(vector.begin(), vector.end()) //retorna o maior valor de um vetor
+
+//min_element(vector.begin(), vector.end()) //retorna o menor valor de um vetor
