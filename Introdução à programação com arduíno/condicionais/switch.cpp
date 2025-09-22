9f87faf4775d3fe638e1613f830a805d66382674
João <jgablopes2008@gmail.com>
2025-09-22 14:52:56 -0300

    Switch

---
+++ Introdução à programação com arduíno/condicionais/switch.cpp
@@ -0,0 +1,29 @@
+//Switch é uma alternativa para o comando if
+
+int dia = 4;
+switch (dia) {
+    case 1:
+        cout << "Segunda-feira";
+        break;
+    case 2:
+        cout << "Terça-feira";
+        break;
+    case 3:
+        cout << "Quarta-feira";
+        break;
+    case 4:
+        cout << "Quinta-feira";
+        break;
+    case 5:
+        cout << "Sexta-feira";
+        break;
+    case 6:
+        cout << "Sábado";
+        break;
+    case 7:
+        cout << "Domingo";
+        break;
+    default:
+        cout << "Dia inexistente";
+        break;
+}
