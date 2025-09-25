43dd7f4c9bb616d2e19aad94be2865aeba9d185d
João <jgablopes2008@gmail.com>
2025-09-25 15:45:49 -0300

    Referências

---
+++ Introdução à programação com arduíno/memory management/referencias.cpp
@@ -0,0 +1,16 @@
+//Referências são armazenadas na memória do dispositivo, variáveis apontam para o local onde essas referências estão armazenadas
+
+//operador & permite criar apelidos para uma variável
+
+string comida = "pizza";
+string &refeicao = comida;
+
+cout << comida << "\n";
+cout << refeicao << "\n";
+
+//atualizar a referência também mudará o valor da variável
+
+refeicao = "Pão";
+
+cout << comida << "\n";
+cout << refeicao << "\n";
