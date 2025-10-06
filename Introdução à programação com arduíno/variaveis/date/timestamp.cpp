6eb699c9041fe930acb1c70d3c048576236cfdac
João <jgablopes2008@gmail.com>
2025-10-06 14:13:50 -0300

    Início date

---
+++ Introdução à programação com arduíno/variaveis/date/timestamp.cpp
@@ -0,0 +1,15 @@
+//Timestamp permite criar uma variável que representa datas e horários
+
+#include <iostream>
+#include <ctime>
+
+using namespace std;
+
+//criando um timestamp da hora e data atual
+
+time_t timestamp;
+time(&timestamp);
+
+//Mostra o tempo atual com base no timestamp criado
+
+cout << ctime(&timestamp);
