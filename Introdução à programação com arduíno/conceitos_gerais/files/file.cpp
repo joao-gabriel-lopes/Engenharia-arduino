eb28efeae43876f5cc7769b05c7e5b653c0abe51
João <jgablopes2008@gmail.com>
2025-10-06 13:54:00 -0300

    Files

---
+++ Introdução à programação com arduíno/conceitos_gerais/files/file.cpp
@@ -0,0 +1,31 @@
+//Arquivos podem ser manipulados com a biblioteca fstream, onde ofstream permite criar e escrever em um arquivo e ifstream permite ler arquivos
+
+#include <iostream>
+#include <fstream>
+
+using namespace std;
+
+// int main() {
+//   //Cria e abre um arquivo de texto
+//   ofstream arquivo("arquivo.txt");
+
+//   //escreve no arquivo
+//   arquivo << "Teste!";
+
+//   //fecha o arquivo
+//   arquivo.close();
+// }
+
+// string texto;
+
+// //lê um arquivo de texto
+// ifstream arquivo("arquivo.txt");
+
+// //usando o getline() para ler o arquivo linha por linha
+// while (getline (arquivo, texto)) {
+//   //Exibe o texto do arquivo
+//   cout << texto;
+// }
+
+// // Close the file
+// arquivo.close();
